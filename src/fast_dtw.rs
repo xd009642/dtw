bd07c06db822a8810072d5b3373ef95342931908
xd009642 <danielmckenna93@gmail.com>
2020-03-03 22:44:17 +0000

    Thinking about things with iterator trait
    Could be interesting...

--- src/fast_dtw.rs
+++ src/fast_dtw.rs
@@ -1,13 +1,22 @@
+use crate::simple::*;
 use crate::types::*;
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, PartialOrd)]
 pub struct FastDtw {
-    radius: f64,
+    radius: usize,
 }
 
 impl FastDtw {
-    pub fn new(radius: f64) -> Self {
+    pub fn new(radius: usize) -> Self {
         Self { radius }
+    }
+
+    fn align_by_iter<It, T, F>(&self, reference: It, observed: It, distance: F) -> Alignments
+    where
+        It: Iterator<Item = T> + Clone,
+        F: Fn(T, T) -> f64,
+    {
+        unimplemented!();
     }
 }
 
@@ -18,6 +27,15 @@ impl DynamicTimeWarping for FastDtw {
     where
         F: Fn(&T, &T) -> f64,
     {
-        unimplemented!()
+        let simple = SimpleDtw::new().build();
+        let min_size = self.radius + 2;
+        if reference.len() <= min_size || observed.len() <= min_size {
+            simple.align(reference, observed, distance)
+        } else {
+            let shrunk_r = reference.iter().step_by(2);
+            let shrunk_o = reference.iter().step_by(2);
+            let low_res = self.align_by_iter(shrunk_r, shrunk_o, distance);
+            unimplemented!()
+        }
     }
 }
